b3650578fa91efa85a201473787175361378d711
takumi0708 <zhangtingtashi@gmail.com>
2026-08-04 03:30:12 +0000

    LIF modle execute

--- main.f90
+++ main.f90
@@ -10,27 +10,39 @@ program main
     real :: input_current !外部から与える刺激
     real :: dt
     real :: tau !膜電位の変化の速さを決める値
+    real :: time !時間
+    real :: threshold ! 発火閾値
 	
 		!静止膜電位を -65 mV とし、最初の膜電位も -65 mV
     resting_voltage = -65.0
     voltage = resting_voltage
 
-    input_current = 20.0
+    input_current = 30.0
     dt = 0.1
     tau = 20.0
+    threshold = -50.0
 
-    n_steps = 100
+    n_steps = 1000
 
-    print *, "step, voltage"
+    print *, "time, voltage"
 		
 		
     do step = 1, n_steps
-		
+
+        time = step * dt
+
+        ! LIF（Leaky Integrate-and-Fire model）モデル　
         voltage = voltage + dt * ( &
             -(voltage - resting_voltage) + input_current &
         ) / tau
 
-        print *, step, voltage
+        print *, time, voltage
+        
+        ! 発火したら膜電位をリセット
+        if(voltage >= threshold) then
+          print *, "spike!"
+          voltage = resting_voltage
+        end if
 
     end do
 
